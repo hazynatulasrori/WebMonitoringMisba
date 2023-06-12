71e23b9697676a2112559e4f26e10efd66c48e8b
hazynatulasrori <hazynatul.asrori@gmail.com>
2023-06-12 23:10:42 +0700

    Menambahkan migration

---
+++ database/migrations/2023_06_12_144220_create_monitoringweb_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('monitoringweb', function (Blueprint $table) {
+            $table->bigIncrements('id_sensor');
+            $table->integer('ph');
+            $table->integer('turbi');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('monitoringweb');
+    }
+};
